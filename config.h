b96e5a8515c5c98dcf0fee1a9c9acc9ffaa7243f
Jonathan Choate <jon@jonathanchoate.xyz>
2022-06-30 13:47:53 -0600

    updated

--- config.h
+++ config.h
@@ -65,9 +65,8 @@ static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
-    //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
-	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
-	{ datetime, "%s",           "%a %b %d %r" },
+	{ run_command, " %4s  ", "pamixer --get-volume-human" },
+	{ cpu_perc,    "[CPU  %s%%]  ", NULL	      },
+	{ ram_perc,    "[RAM  %s%%]  ", NULL	      },
+	{ datetime, "%s",           "%a %b %d %R" },
 };
